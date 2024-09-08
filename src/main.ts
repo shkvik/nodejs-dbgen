704e235d16b7ee3ba5ec6189291ded42c7495b1b
pendulum <rs.pendulum@gmail.com>
2024-09-08 21:24:36 +0300

    created function for employes

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { faker } from '@faker-js/faker';
 import { DataSource, DeepPartial, Entity, EntityTarget, ObjectLiteral, Repository } from 'typeorm';
 import { Sales } from './pg_schema/entities/Sales';
 import { SingleBar, Presets } from 'cli-progress'
+import { Employees } from './pg_schema/entities/Employees';
+import { Departments } from './pg_schema/entities/Departments';
+import { Teams } from './pg_schema/entities/Teams';
 
 declare global {
   interface Function {
@@ -15,23 +18,6 @@ Function.prototype.bindNext = function (thisArg: any, ...argArray: any[]) {
   return this.bind(null, thisArg, ...argArray);
 };
 
-function createTestUsers(userRepository: Repository<Users>) {
-  const result = new Array(1000)
-    .fill(null)
-    .map(() => new Array<Users>(1000));
-
-  for (let i = 0; i < 1000; i++) {
-    for (let j = 0; j < 1000; j++) {
-      const user = userRepository.create({
-        username: faker.person.fullName(),
-        email: faker.internet.email(),
-        password: faker.internet.password()
-      });
-      result[i][j] = user;
-    }
-  }
-  return result;
-}
 
 type DeepStructure<T> = T | (T extends Array<infer U> ? DeepStructure<U>[]
   : T extends Map<infer K, infer V> ? Map<DeepStructure<K>, DeepStructure<V>>
@@ -41,23 +27,28 @@ type DeepStructure<T> = T | (T extends Array<infer U> ? DeepStructure<U>[]
 
 
 type DeepCallbacks<T> = {
-  [K in keyof Omit<T, 'id'>]: (...args: any[]) => DeepStructure<T[K]>;
+  [K in keyof Omit<T, 'id'>]?: (...args: any[]) => DeepStructure<T[K]>;
 }
 
 
 function createEntity<Entity>(targetEntity: new () => Entity) {
-  return function (params: { dataSource: DataSource, matrix: number }) {
+  return function (params: { dataSource: DataSource, length: number }) {
     return async function createFakeSales(entityCallbacks: DeepCallbacks<Entity>) {
+      const limit = 1000;
+      const { length } = params;
+      const barLimit = Math.ceil(length / limit);
       const creatingBar = new SingleBar({
-        format:  `Creating of ${targetEntity.name} | {bar} | {percentage}% || {value}/{total} Chunks`,
+        format: `Creating of ${targetEntity.name} | {bar} | {percentage}% || {value}/{total} Chunks`,
       }, Presets.legacy);
-      creatingBar.start(params.matrix, 0);
+      creatingBar.start(barLimit, 0);
       const repository = dataSource.getRepository(targetEntity);
-      const raws = new Array(params.matrix).fill(null).map(
-        () => new Array<Entity>(params.matrix)
+      const raws = new Array(Math.ceil(length / limit)).fill(null).map(
+        (_, index) => new Array<Entity>(
+          (length / (index + 1)) > limit ? limit : length - (limit * index)
+        )
       );
-      for (let i = 0; i < params.matrix; i++) {
-        for (let j = 0; j < params.matrix; j++) {
+      for (let i = 0; i < Math.ceil(length / limit); i++) {
+        for (let j = 0; j < raws[i].length; j++) {
           let obj: Partial<Entity> = {};
           for (const key in entityCallbacks) {
             if (typeof entityCallbacks[key] === 'function') {
@@ -70,21 +61,43 @@ function createEntity<Entity>(targetEntity: new () => Entity) {
         }
       }
       creatingBar.stop();
-      const UploadingBar = new SingleBar({
-        format:  `Uploading of ${targetEntity.name} | {bar} | {percentage}% || {value}/{total} Chunks`,
+      const uploadingBar = new SingleBar({
+        format: `Uploading of ${targetEntity.name} | {bar} | {percentage}% || {value}/{total} Chunks`,
       }, Presets.legacy);
-      UploadingBar.start(params.matrix, 0);
+      uploadingBar.start(barLimit, 0);
+      const result: Entity[] = [];
       const promises = raws.map(async (raw) => {
-        await repository.save(raw);
-        UploadingBar.increment();
+        const dbRaw = await repository.save(raw);
+        result.push(...dbRaw);
+        uploadingBar.increment();
       });
       await Promise.all(promises);
-      UploadingBar.stop();
+      uploadingBar.stop();
+      return result;
+
     }
   }
 }
 
+async function createEmployees() {
+  // const employees = dataSource.getRepository(Employees);
+  // const teams = dataSource.getRepository(Teams);
+  // const departments = dataSource.getRepository(Departments);
 
+  const departments = await createEntity(Departments)({ dataSource, length: 1000 })({
+    departmentName: faker.person.jobType,
+  });
+  const teams = await createEntity(Teams)({ dataSource, length: 1000 })({
+    teamName: faker.person.jobArea
+  });
+  return createEntity(Employees)({ dataSource, length: 1_000_000 })({
+    name: faker.person.firstName,
+    department: faker.person.jobType,
+    salary: faker.number.int.bindNext({ min: 0, max: 10000, multipleOf: 1000 }),
+    department_2: faker.helpers.arrayElement.bindNext(departments),
+    team: faker.helpers.arrayElement.bindNext(teams)
+  });
+}
 
 
 async function main() {
@@ -94,19 +107,5 @@ async function main() {
     .catch((err) => {
       console.error("Error during Data Source initialization", err)
     });
-
-  await createEntity(Sales)({ dataSource, matrix: 1000 })({
-    amount: faker.number.int.bindNext({ min: 0, max: 1_000_000 }),
-    productId: faker.number.int.bindNext({ min: 1, max: 100 }),
-    saleDate: faker.date.between.bindNext({ from: '2020-01-01T00:00:00.000Z', to: '2023-01-01T00:00:00.000Z' }),
-  });
-  // const fakeUsres = createTestUsers(userRepository);
-
-  // for(const usresChunk of fakeUsres){
-  //   await userRepository.save(usresChunk);
-  // }
-
-  console.log('users')
-
 }
 main()
