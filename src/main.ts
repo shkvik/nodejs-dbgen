5013fa7f1fc8b4c7203104b61d6853f260b9dae8
pendulum <rs.pendulum@gmail.com>
2024-09-08 18:12:25 +0300

    added generic function

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { Users } from './pg_schema/entities/Users'
 import { dataSource } from './config'
 import { faker } from '@faker-js/faker';
-import { Repository } from 'typeorm';
+import { DataSource, DeepPartial, Entity, EntityTarget, ObjectLiteral, Repository } from 'typeorm';
+import { Sales } from './pg_schema/entities/Sales';
 
 function createTestUsers(userRepository: Repository<Users>) {
   const result = new Array(1000)
@@ -9,7 +10,7 @@ function createTestUsers(userRepository: Repository<Users>) {
     .map(() => new Array<Users>(1000));
 
   for (let i = 0; i < 1000; i++) {
-    for(let j = 0; j < 1000; j++){
+    for (let j = 0; j < 1000; j++) {
       const user = userRepository.create({
         username: faker.person.fullName(),
         email: faker.internet.email(),
@@ -21,6 +22,47 @@ function createTestUsers(userRepository: Repository<Users>) {
   return result;
 }
 
+type DeepStructure<T> = T | (T extends Array<infer U> ? DeepStructure<U>[]
+  : T extends Map<infer K, infer V> ? Map<DeepStructure<K>, DeepStructure<V>>
+  : T extends Set<infer M> ? Set<DeepStructure<M>> : T extends object ? {
+    [K in keyof T]?: DeepStructure<T[K]>;
+  } : T);
+
+
+type DeepCallbacks<T> = {
+  [K in keyof Omit<T, 'id'>]: (...args: any[]) => DeepStructure<T[K]>;
+}
+
+
+function createEntity<Entity>(targetEntity: new () => Entity) {
+  return function (dataSource: DataSource) {
+    return async function createFakeSales(entityCallbacks: DeepCallbacks<Entity>) {
+      const repository = dataSource.getRepository(targetEntity);
+      const raws = new Array(1000)
+        .fill(null)
+        .map(() => new Array<Entity>(1000));
+
+      for (let i = 0; i < 1000; i++) {
+        for (let j = 0; j < 1000; j++) {
+          let obj: Partial<Entity> = {};
+          for (const key in entityCallbacks) {
+            if (typeof entityCallbacks[key] === 'function') {
+              obj[key as keyof Entity] = entityCallbacks[key]();
+            }
+          }
+          const raw = repository.create(obj as Entity);
+          raws[i][j] = raw;
+        }
+      }
+      for (const sale of raws) {
+        await repository.save(sale);
+      }
+    }
+  }
+}
+
+
+
 
 async function main() {
   await dataSource.initialize()
@@ -28,14 +70,19 @@ async function main() {
     .catch((err) => {
       console.error("Error during Data Source initialization", err)
     });
-  
-  const userRepository = dataSource.getRepository(Users)
-  
-  const fakeUsres = createTestUsers(userRepository);
 
-  for(const usresChunk of fakeUsres){
-    await userRepository.save(usresChunk);
-  }
+  const userRepository = dataSource.getRepository(Users)
+
+  const fakeSales = await createEntity(Sales)(dataSource)({
+    amount: faker.number.int.bind(null, { min: 0, max: 1_000_000 }),
+    productId: faker.number.int.bind(null, { min: 1, max: 100 }),
+    saleDate: faker.date.between.bind(null, { from: '2020-01-01T00:00:00.000Z', to: '2023-01-01T00:00:00.000Z' }),
+  });
+  // const fakeUsres = createTestUsers(userRepository);
+
+  // for(const usresChunk of fakeUsres){
+  //   await userRepository.save(usresChunk);
+  // }
 
   console.log('users')
 
